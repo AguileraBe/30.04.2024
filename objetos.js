84e5398c255a707a2a1378ba9e121dee42c465bc
Be <159550318+AguileraBe@users.noreply.github.com>
2024-04-30 11:48:19 -0300

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,12 @@
+class Calebe {
+    constructor (site){
+        this.site = site;
+    }
+
+    exibirSite (){
+        window.open(this.site);
+    }
+}
+
+var Calebe = new Calebe ("https://www.youtube.com/@Beaguilera");
+novoObjeto.metodoQueVocêCriou();
